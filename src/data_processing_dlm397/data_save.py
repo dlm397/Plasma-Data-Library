5d1840e0aae599289ed33a4ff1c499d7c4839482
dlm397 <dlm397@drexel.edu>
2025-07-28 19:04:41 -0400

    Silenced module import errors.

--- src/data_processing_dlm397/data_save.py
+++ src/data_processing_dlm397/data_save.py
@@ -1,5 +1,5 @@
 from config_loader import load_config, get_path, get_setting, get_home_path
-import numpy as np
+import numpy as np # type: ignore
 
 def save_data(data, file_name, path_key="data_dir"):
     save_location = get_path(path_key)
--- src/data_processing_dlm397/example.py
+++ src/data_processing_dlm397/example.py
@@ -1,5 +1,5 @@
 from data_save import save_data
 from config_loader import load_config, get_path, get_setting
-import numpy as np
+import numpy as np # type: ignore
 
 print(save_data([1,2,3,4], "test")) # np.random.rand(10,10)
